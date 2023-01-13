124f848274ba4dd87b844ffad145235ebe8fbd8a
Danilo Campanhari <danilocampanhari64@gmail.com>
2023-01-13 16:07:53 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'dan aqui'),
+      home: const MyHomePage(title: 'test'),
     );
   }
 }
